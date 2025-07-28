3aaa124135e6309ba79aedf20afeb5c02435c9f0
Ebit22 <tibe.naveauprincen@gmail.com>
2025-07-28 16:26:34 +0200

    Level 7 done

--- gravityplatformer/Scripts/killzone.gd
+++ gravityplatformer/Scripts/killzone.gd
@@ -1,7 +1,20 @@
 extends Area2D
 
+var bodies_in_zone = []
 
-
+func _ready() -> void:
+	visible = true
 
 func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://Scenes/Menu's/restart_screen.tscn")
+	bodies_in_zone.append(body)
+	print(visible, body)
+	if body.is_in_group("player") and visible == true:
+		get_tree().change_scene_to_file("res://Scenes/Menu's/restart_screen.tscn")
+	else: 
+		visible = bodies_in_zone.size() == 0
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	bodies_in_zone.erase(body)
+	visible = bodies_in_zone.size() == 0
